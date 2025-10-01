aa67e4163e8a3b087572716b0d3e597b60bbe192
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2025-10-02 05:43:55 +0800

    Remove `paths.base` from svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -33,9 +33,6 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    },
 		prerender: {
 			handleHttpError: "warn",
 		}
