5663d8e127dd0fa8f53e356cf8fbc7110aa5d2cb
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2026-02-13 09:10:45 +0800

    Renderer: Fix Previewer not dynamic

--- src/routes/preview/+page.ts
+++ src/routes/preview/+page.ts
@@ -2,6 +2,8 @@ import { error } from '@sveltejs/kit';
 import type { PageLoadEvent } from './$types';
 import { concatAndResolveUrl, parseEfp, PreviewEfpPathResolver, type PreviewEfpAssetFetcher } from "$lib/efp";
 
+export const ssr = false;
+
 export async function load({ fetch, url }: PageLoadEvent) {
 	if (url.searchParams.get("source") === "github") {
 		const raw = url.searchParams.get("raw");
