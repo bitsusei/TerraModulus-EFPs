849f4acfc84ba4544400a845e5c531b79e6eb2b1
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2026-04-26 10:31:55 +0800

    Renderer: Add ability to try availability of EFP

--- src/routes/preview/+page.ts
+++ src/routes/preview/+page.ts
@@ -1,10 +1,18 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageLoadEvent } from './$types';
 import { concatAndResolveUrl, parseEfp, PreviewEfpPathResolver, type PreviewEfpAssetFetcher } from "$lib/efp";
 
 export const ssr = false;
 
-export async function load({ fetch, url }: PageLoadEvent) {
+export async function load({ fetch, url, parent }: PageLoadEvent) {
+	if (url.searchParams.get("trial") !== null) {
+		const id = url.searchParams.get("trial")!
+		if ((await parent()).map[id] !== undefined) {
+			const target = new URL("/efp/" + id, url)
+			throw redirect(303, target);
+		}
+	}
+	
 	if (url.searchParams.get("source") === "github") {
 		const raw = url.searchParams.get("raw");
 		if (raw === null) throw error(400, "Bad Request");
